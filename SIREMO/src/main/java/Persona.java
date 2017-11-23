050e2edcc3d86e3e5b61d8a17dbbd8ee28222c42
root <root@kali>
2017-11-23 18:18:06 -0500

    ok

--- SIREMO/src/main/java/Persona.java
+++ SIREMO/src/main/java/Persona.java
@@ -3,7 +3,8 @@ public class Persona {
 
 	
 	public void Persona() {
-		System.out.println("Acabo de enviar un commit y push!");
+		System.out.println(" ok oko ko kkook okko k push!");
 			System.out.println("Commit");
+			
 	}
 }
